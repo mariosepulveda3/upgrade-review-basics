7f25be86c02e47f501a131d35588c32bb560ee18
mariosepulveda3 <mariosepul3@gmail.com>
2022-10-10 10:49:53 +0200

    CORRECTED, exercise 4

--- iteración-4.js
+++ iteración-4.js
@@ -2,14 +2,37 @@
 
 // **Iteración #4: Métodos findArrayIndex**
 
-// Crea una función llamada `findArrayIndex` que reciba como parametros un array de textos 
-// y un texto y devuelve la posición del array cuando el valor del array sea igual al valor del 
-// texto que enviaste como parametro. Haz varios ejemplos y compruebalos.
+// Crea una función llamada `findArrayIndex` que reciba como 
+// parametros un array de textos 
+// y un texto y devuelve la posición del array cuando el 
+// valor del array sea igual al valor del 
+// texto que enviaste como parametro. Haz varios ejemplos
+//  y compruebalos.
 
 // Sugerencia de función:
-
-function findArrayIndex(array, text) {}
-
+// function findArrayIndex(array, text) {}
 // Ej array:
+// ['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
 
-['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
+const animals = ['Caracol', 'Mosquito', 'Salamandra', 'Ajolote'];
+
+function findArrayIndex (array, text) {
+    //* Como se debía hacer, con un indexOf
+    // return array.indexOf(text);
+
+    //* Haciendolo con for
+     for (let i = 0; i < array.length; i++) {
+         if (array[i] === text) {
+             return i;
+         }
+     }
+}
+
+console.log('Caracol tiene valor :');
+console.log(findArrayIndex(animals, 'Caracol'));
+console.log('Mosquito tiene valor :');
+console.log(findArrayIndex(animals, 'Mosquito'));
+console.log('Salamandra tiene valor :');
+console.log(findArrayIndex(animals, 'Salamandra'));
+console.log('Ajolote tiene valor :');
+console.log(findArrayIndex(animals, 'Ajolote'));
